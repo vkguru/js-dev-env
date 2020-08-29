c3127d8afb18d0b588018d22acfdef4ae9bcb283
vkguru <vnwoguru@gmail.com>
2020-08-29 19:56:17 +0100

    HTTP request using Mock API and Json Schema

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
-import './index.css';
-import numeral from 'numeral';
+import {getUser, deleteUser} from './api/userApi'
 
-const courseValue = numeral(1000).format('$0,0.00');
+// populate the table users via API call
 
-/* eslint-disable no-console*/
-console.log(`I would pay ${courseValue} for the awesome course!`);
+getUser().then(result => {
+  let usersBody = "";
+
+  result.forEach(user => {
+    usersBody += `<tr>
+        <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</td>
+        <td>${user.id}</td>
+        <td>${user.firstName}</td>
+        <td>${user.lastName}</td>
+        <td>${user.email}</td>
+      </tr>
+    `
+  });
+
+  global.document.getElementById('users').innerHTML = usersBody;
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+  // Must use array.from to create a real array from a DOM collection
+  // getElementsByClassname only returns an "array like" object
+  Array.from(deleteLinks, link => {
+    link.onclick = function(event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    };
+  });
+});
